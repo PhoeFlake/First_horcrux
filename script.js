96d8f130d691acb384ad016327db9e07b7683a1b
PhoeFlake <drishtimadaan2004@gmail.com>
2024-02-03 14:45:42 +0530

    lets chill

---
+++ script.js
@@ -0,0 +1,10 @@
+var button = document.getElementById('demobutton1');
+
+button.addEventListener('click', function(){
+     alert("Welcome to the club");
+});
+var button = document.getElementById('demobutton2');
+
+button.addEventListener('click', function(){
+     alert("Maa chuda bsdk");
+});
